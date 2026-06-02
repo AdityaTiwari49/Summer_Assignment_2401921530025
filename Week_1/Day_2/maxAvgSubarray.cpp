08734fee7e541abee5f6ce15e25e53179c376670
Aditya Tiwari <aditiwari327@gmail.com>
2026-06-02 21:11:58 +0530

    day 2 problem solved

---
+++ Week_1/Day_2/maxAvgSubarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        long long Sum = 0;
+
+        
+        for (int i = 0; i < k; i++) {
+            Sum += nums[i];
+        }
+
+        long long maxSum = Sum;
+
+        
+        for (int i = k; i < nums.size(); i++) {
+            Sum += nums[i];
+            Sum -= nums[i - k];
+
+            maxSum = max(maxSum,Sum);
+        }
+
+        return (double)maxSum / k;
+    }
+};
