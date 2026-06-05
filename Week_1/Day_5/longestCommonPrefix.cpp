0a6377e7b768c8cbeddc2895f8b62ad91475f84e
Aditya Tiwari <aditiwari327@gmail.com>
2026-06-05 15:51:56 +0530

    Add implementation of longestCommonPrefix and reverseString functions

---
+++ Week_1/Day_5/longestCommonPrefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        if (strs.empty())
+            return "";
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+        int minilength = min(first.size(), last.size());
+
+        for (int i = 0; i < minilength; i++) {
+            if (first[i] != last[i])
+                break;
+            else {
+                ans += first[i];
+            }
+        }
+        return ans;
+    }
+};
