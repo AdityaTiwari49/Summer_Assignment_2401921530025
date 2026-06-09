54c70efe8de9eb247b8951293a2e3839ba0728cc
Aditya Tiwari <aditiwari327@gmail.com>
2026-06-09 23:46:47 +0530

    Add implementation of findAnagrams function to identify all anagrams of a string within another string

---
+++ Week_2/Day_2/all_anagrams.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+        vector<int> ans;
+
+        if (p.size() > s.size())
+            return ans;
+
+        vector<int> need(26, 0);
+        vector<int> window(26, 0);
+
+
+        for (char ch : p)
+            need[ch - 'a']++;
+
+        int k = p.size();
+
+        
+        for (int i = 0; i < k; i++)
+            window[s[i] - 'a']++;
+
+        if (window == need)
+            ans.push_back(0);
+
+    
+        for (int i = k; i < s.size(); i++) {
+
+            window[s[i] - 'a']++;        
+            window[s[i - k] - 'a']--;     
+
+            if (window == need)
+                ans.push_back(i - k + 1);
+        }
+
+        return ans;
+    }
+};
